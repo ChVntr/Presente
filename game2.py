47ae5577087774f5ab701956f3cba3bc0a8c3409
Ch V. <cahiosantos11+git@gmail.com>
2025-04-28 23:33:05 -0300

    eu fiz tanta coisa que nem lembro o que documentar

--- game2.py
+++ game2.py
@@ -34,7 +34,6 @@ def loop_geral():
 def sair():
 
     for event in event_buffer_get(hold=True):
-        print(event)
         if event.type == pygame.QUIT:            
             pygame.quit()
             quit()
@@ -46,7 +45,7 @@ def guia_de_cenas(cena):
         if save_existe():
             with open('save', 'r') as f:
                 data = f.readlines()
-                if data[0] == '00':
+                if data[0] == '00' or data[0] == '':
                     f.close()
                     return 1
         else:
@@ -54,16 +53,20 @@ def guia_de_cenas(cena):
     elif cena == 1:
         criar_save()
         return 2    
+    elif cena == 2:
+        level_001()
+        return 2
 
 def criar_save():
 
     with open('save', 'w') as f:
         f.write('00')
-        f.close
+        f.close()
 
     text = ''
     linha_digitação = True
     start = time.perf_counter()
+    quebra = False
     while True:
 
         fonte_tamanho = fonte_padrao().get_height()
@@ -73,15 +76,17 @@ def criar_save():
         draw_text(texto[0], posicao_do_texto[0], posicao_do_texto[1])
 
         for event in event_buffer_get():
-            if event.type == pygame.KEYDOWN:
-                if event.key in teclas_confirmar:
-                    print('deu enter')
-                elif event.key == pygame.K_BACKSPACE:
-                    text = text[:-1]
-            elif event.type == pygame.TEXTINPUT:
+            if event.type == pygame.TEXTINPUT:
                 text += event.text
                 linha_digitação = True
                 start = time.perf_counter()
+            elif event.type == pygame.KEYDOWN:
+                if event.key in teclas_confirmar:
+                    if text != '':
+                        print('enter')
+                        quebra = True
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
                 
         text_2 = text
 
@@ -97,6 +102,12 @@ def criar_save():
 
 
         loop_geral()
+
+        if quebra: break
+
+    with open('save', 'w') as f:
+        f.writelines(text,)
+        f.close()
 
 def draw_text(texto, x, y, fonte = None, cor_do_texto = None, menos_um = None):
 
@@ -122,6 +133,7 @@ def event_buffer():
     for event in pygame.event.get():
         ev_buffer.append(event)
 
+    #print(event)
     return ev_buffer
 
 def event_buffer_get(hold = None):
@@ -135,9 +147,37 @@ def event_buffer_get(hold = None):
 
     return retornar
     
+def level_001():
+
+    chao_x = 300
+    chao_y = 600
+
+    chao = ground(chao_x, chao_y)
+    for repet in range(1, 15):
+        ground((chao_x + repet * chao.rect.width), chao_y)
+
+    eu = player(400, 500)
 
 
 
+#classes (ainda não sei o que tô fazendo)
+class ground(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprite = pygame.image.load('sprites/place-holder_chao_armored-armadillo.png')
+        self.rect = self.sprite.get_rect()
+        self.rect.center = (x, y)
+        screen.blit(self.sprite, self.rect)
+    
+class player(pygame.sprite.Sprite):
+    def __init__(self, x, y, velocidade):
+        pygame.sprite.Sprite.__init__(self)
+        self.velocidade = velocidade
+        self.sprite = pygame.image.load('sprites/place-holder_Sara.png')
+        self.rect = self.sprite.get_rect()
+        self.rect.center = (x, y)
+        screen.blit(self.sprite, self.rect)
+        
 
 #agrupamentos de teclas
 teclas_confirmar = (pygame.K_KP_ENTER, pygame.KSCAN_KP_ENTER, pygame.K_RETURN, pygame.KSCAN_RETURN)
@@ -150,6 +190,7 @@ teclas_baixo = (pygame.K_DOWN, pygame.K_s)
 
 #inicialização do pygame 
 pygame.init()
+pygame.display.set_caption('Presente')
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((1600, 900), pygame.RESIZABLE)
